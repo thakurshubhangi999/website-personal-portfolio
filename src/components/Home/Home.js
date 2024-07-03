70ba480e260b4a3b3290557550491ac326e937cb
Shubhangi Thakur <thakurshubhangi999@gmail.com>
2024-07-04 00:37:49 +0530

    Complete code for portfolio website

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,27 @@
 import React from "react";
 import "./Home.css"; // Import the CSS file
-import { getImageUrl } from "../../utils";
+import heroImage from "./heroImage.png";
 
 export const Home = () => {
   return (
     <section className="container">
       <div className="content">
-        <h1 className="title">Hi, I'm Ada</h1>
+        <h1 className="title">Hi, I'm Shubhangi</h1>
+        <h1 className="intro">Welcome to my portfolio website</h1>
         <p className="description">
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a Frontend Engineer with 2 years of experience specializing in
+          responsive web design and front-end development using React and React
+          UI frameworks.
+        </p>
+        <p className="description">
+          I have professional expertise in HTML, CSS, and JavaScript, creating
+          visually appealing and intuitive websites with ReactJS.
         </p>
         <a href="mailto:myemail@email.com" className="contactBtn">
           Contact Me
         </a>
       </div>
-      <img
-        src={getImageUrl("hero/heroImage.png")}
-        alt="Hero image of me"
-        className="heroImg"
-      />
+      <img src={heroImage} alt="Hero image of me" className="profile-image" />
       <div className="topBlur" />
       <div className="bottomBlur" />
     </section>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -26,6 +26,9 @@ export const Navbar = () => {
           onClick={() => setMenuOpen(false)}
         >
           <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
             <a href="#about">About</a>
           </li>
           <li>
