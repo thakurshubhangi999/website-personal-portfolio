4f408a8a5ce5dd22e3fa1be7a38474ed4e0d1a5b
Shubhangi Thakur <thakurshubhangi999@gmail.com>
2024-09-26 22:56:30 +0530

    converted pixxel values into rem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,17 +9,14 @@ export const Home = () => {
         <h1 className="title">Hello, I'm Shubhangi</h1>
         <p className="description">Welcome to my portfolio website</p>
         <p className="description">
-          I am a qualified Frontend Engineer with over 2 years of experience
-          specializing in responsive web design and front-end development. My
-          expertise lies in utilizing HTML, CSS, and JavaScript to create
-          visually appealing and user-friendly websites. Proficient in ReactJS
-          and Material-UI, I focus on driving customer engagement and
-          contributing to overall business success.
+          I am a qualified Frontend Engineer with over 2 years of experience in
+          responsive web design and front-end development, specializing ReactJS
+          and React UI Frameworks.
         </p>
         <p className="description">
-          I am passionate about embracing new opportunities, expanding my skill
-          set in web development, and creating impactful solutions that enhance
-          user experience.
+          I focus on creating visually appealing websites that drive customer
+          engagement and enhance user experience while seeking opportunities to
+          expand my skills and create impactful solutions.
         </p>
         <p className="description">
           Over the course of my career, I have had the opportunity to work at
