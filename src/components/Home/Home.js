e4666a210e3832ea9d1a9680cfecf3603d128039
Shubhangi Thakur <thakurshubhangi999@gmail.com>
2024-09-27 22:53:24 +0530

    for 1024, 830px fixed padding, margin of home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,12 +8,9 @@ export const Home = () => {
     <section className="container">
       <div className="content">
         <h1 className="title">Hi, I'm Shubhangi</h1>
-        {/* <h3 className="title">Frontend Engineer</h3> */}
-        <Typewriter className="title" text="Frontend Engineer" speed={150} />
-        <p className="description">I build things for the web.</p>
-        <p className="description">
-          I like working on the front-end of the web.
-        </p>
+        <Typewriter text="Frontend Engineer" speed={150} />
+        <p className="description">I like to craft solid and scalable frontend products with great user experience.</p>
+        <p className="description">Uncover my career journey 👇</p>
         <a
           href="https://drive.google.com/uc?export=download&id=1hAbPYnocU4amrLjGPXKV7uue5HzDxGU5"
           className="contactBtn"
